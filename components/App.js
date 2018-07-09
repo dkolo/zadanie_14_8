ec73e99873c28773cbcb4f44d3aea46a3c51f51b
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-07-09 11:03:09 +0200

    Remove undefined "GIPHY_PUB_KEY" variable

--- components/App.js
+++ components/App.js
@@ -22,7 +22,7 @@ App = React.createClass({
 
     getGif: function (searchingText, callback) {
         var GIPHY_API_URL = 'http://api.giphy.com';
-        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+        var url = GIPHY_API_URL + '/v1/gifs/random' + '&tag=' + searchingText;
         var xhr = new XMLHttpRequest();
         xhr.open('GET', url);
         xhr.onload = function () {
